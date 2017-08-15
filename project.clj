b11f2677cc6dfd489f9071a98597d66d07e755ef
Jeb Beich <jebbeich@gmail.com>
2017-08-15 15:47:39 -0500

    Cleanup project

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject lambda-sample "0.1.0"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [com.amazonaws/aws-lambda-java-core "1.1.0"]]
-  #_#_:java-source-paths ["src/java"]
-  :aot :all
-  )
+  :aot :all)
